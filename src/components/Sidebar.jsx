e3679a3549d82a63f275460c69e232141895b583
Tessa <new@news-mbp-426.home>
2026-06-08 11:58:44 -0400

    Add sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,48 @@
+import { useClerk, useUser } from '@clerk/clerk-react'
+import { Eraser, FileText, Hash, Home, Icon, Image, LogOut, Scissors, SquarePen, Users } from 'lucide-react'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const navItems = [
+    {to:'/ai', title: 'Dashboard', Icon: Home},
+    {to:'/ai/write-article', title:'Write Article',Icon:SquarePen},
+    {to:'/ai/blog-titles', title:'Block Title',Icon:Hash},
+    {to:'/ai/generate-images', title:'Generate Images',Icon:Image},
+    {to:'/ai/remove-background', title:'Remove Background',Icon:Eraser},
+    {to:'/ai/remove-object', title:'Remove Object',Icon:Scissors},
+    {to:'/ai/review-resume', title:'Review Resume',Icon:FileText},
+    {to:'/ai/community', title:'Community', Icon:Users}
+]
+const Sidebar = ({sidebarOpen,setSidebarOpen}) => {
+    const {user} = useUser()
+    const {signOut, openUserProfile} = useClerk()
+
+    return (
+    <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${sidebarOpen? 'translate-x-0':'max-sm:translate-x-full'} transition-all duration-300 ease-in-out`}>
+        <div className="my-7 w-full px-1">
+            <img src={user.imageUrl} className="w-13 rounded-full mx-auto"/>
+            <h1 className="mt-1 text-center cursor-pointer" onClick={openUserProfile} >{user.fullName}</h1>
+            <div className="px-6 mt-5 text-sm text-gray-500 font-medium">
+
+            {navItems.map(({to, title, Icon})=>(
+                <NavLink key={to} to={to} end = {to ==="/ai"} onClick={setSidebarOpen(false)} className={({isActive})=>`px-3 py-2.5 flex items-center gap-3 rounded ${isActive ? 'bg-gradient-to-r from-[#3C81F6] to-[#9243E3] text-white': ''}`}>
+                    {({isActive})=>(
+                        <>
+                        <Icon className={`h-4 w-4 ${ isActive ? 'text-white': ''}`}/>
+                        {title}
+                        </>
+                    )}
+                </NavLink>
+            ))}
+            </div>
+        </div>
+        <div className='w-full border-t border-gray-200 p-4 px-7 flex items-center justify-between'>
+            <img src={user.imageUrl} className='w-8 rounded-full' alt="user image"/>
+            <h1 className='text-sm font-medium cursor-pointer' onClick={openUserProfile}>{user.fullName}</h1>
+            <LogOut onClick={signOut} className='w-4.5 text-gray-400 hover:text-gray-700 transition cursor-pointer'/>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { Menu, X } from "lucide-react";
+import Sidebar from "../components/Sidebar";
+import {SignIn, useUser} from '@clerk/clerk-react'
 
 const Layout = () => {
-  return (
-    <div>
-      <h1>Layout</h1>
+  const navigate = useNavigate();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const {user} = useUser()
+
+  return user ? (
+    <div className="flex flex-col items-start justify-start h-screen">
+      <nav className="w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200">
+        <img
+          src={assets.logo}
+          alt="Logo"
+          onClick={() => navigate("/")}
+          className="cursor-pointer"
+        />
+        {sidebarOpen ? (
+          <X
+            className="h-6 w-6 text-gray-500 sm:hidden"
+            onClick={() => setSidebarOpen(false)}
+          />
+        ) : (
+          <Menu
+            className="h-6 w-6 text-gray-500 sm:hidden"
+            onClick={() => setSidebarOpen(true)}
+          />
+        )}
+      </nav>
+      <div className="flex flex-1 w-full h-[calc(100vh-64px)]">
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div className="flex-1 bg-[#F4F7FB]">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ):(
+    <div className="flex items-center justify-center h-screen">
+      <SignIn/>
     </div>
   );
 };
