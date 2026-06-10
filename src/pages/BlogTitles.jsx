969307a43dece67dc4d35c71cd4b17a24f88baf9
Tessa <new@fa:51:48:1c:69:6b.home>
2026-06-10 12:54:25 -0400

    Add UI for blog titles

--- src/pages/BlogTitles.jsx
+++ src/pages/BlogTitles.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import { Hash, Sparkles } from "lucide-react";
+import React, { useState } from "react";
 
+const category = [
+  "General",
+  "Technology",
+  "Bussiness",
+  "Health",
+  "Lifestyle",
+  "Education",
+  "Food",
+  "Travel",
+];
 const BlogTitles = () => {
-  return (
-    <div><h1>Blog Titles</h1></div>
-  )
-}
+  const [catSelected,setCatSelected]=useState(category[0])
 
-export default BlogTitles
+  const onSubmitHandler = async (e) =>{
+    e.preventDefault()
+  }
+  const [input, setInput]= useState('')
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* Col 1 */}
+      <form onSubmit={onSubmitHandler} className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200">
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#8E37EB]" />
+          <h1 className="font-semibold text-lg">AI Article Generator</h1>
+        </div>
+        <h2 className="mt-6 text-sm font-medium">Keyword</h2>
+        <input
+          type="text"
+          onChange = {(e)=> setInput(e.target.value)}
+          value = {input}
+          placeholder="The future of AI"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          required
+        />
+        <h2 className="mt-4 text-sm font-medium">Category</h2>
+        <div className="flex flex-wrap gap-3 mt-3 sm:max-w-9/11">
+          {
+            category.map((cat)=>(
+              <div key={cat} 
+              onClick={()=>setCatSelected(cat)}
+              className={`px-4 py-1 text-xs rounded-full border cursor-pointer ${catSelected === cat ? 'bg-purple-50 text-purple-700':'border-gray-300 text-gray-500'}`}>{cat}</div>
+            ))
+          }
+        </div>
+        <button className="w-full flex items-center justify-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Hash className="w-5"/>
+          Generate Title</button>
+      </form>
+      {/* Col 2 */}
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+        <div className="flex items-center gap-3">
+          <Hash className="w-5 h-5 text-[#8E37EB]"/>
+          <h1 className="text-sl font-semibold">Generated Titles</h1>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Hash className="w-9 h-9"/>
+            <p>Enter a topic and click "Generate Title" to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogTitles;
